f88cb74254e85b4cb4dd6b3080a4c82a839a5fc4
kangyupeng <kangyupeng@goatai.cn>
2022-08-19 16:43:10 +0800

    支持多页面，支持处理css并提取css文件，支持图片处理，支持开发时修改文件，浏览器实时刷新

---
+++ src/pages/page2/index.js
@@ -0,0 +1,4 @@
+import '../../static/css/font.css'
+import './index.css'
+
+console.log('哈哈哈')
--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,71 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin'); //这个插件，可将css提成文件，而不是把css放进html里
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); //这个插件，可将css提成文件，而不是把css放进html里
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 //webpack配置
 module.exports = {
     //入口文件
     entry: {
-        page1: "./src/pages/page1/index.js",
-        page2: "./src/pages/page2/index.js"
+        "page1": "./src/pages/page1/index.js",
+        "page2": "./src/pages/page2/index.js",
+        /*问：这里为什么要把html和css文件添加到入口文件这里？
+        答：为了，在开发时，修改文件内容后，浏览器内容能实时更新，
+        webpack的devServer仅监听，入口文件，的变化。而且想实现修改文件后，
+        浏览器自动刷新，还需要在derServer的配置里写一个，watchFials属性，二者缺一不可。
+        默认情况下，如果这里不写html和css，只写页面入口js文件的话，只有修改js文件时，浏览器才会自动刷新
+        */
+        "page1.html": "./src/pages/page1/index.html",
+        "page1.css": "./src/pages/page1/index.css",
+        "page2.html": "./src/pages/page2/index.html",
+        "page2.css": "./src/pages/page2/index.css",
     },
     //输出
     output: {
         //输出文件名   [name] 为 entry 中的 key 如page1:...中的page1
-        filename: 'js/[name]/index.js', //输出到build文件下，js文件夹内
+        filename: 'js/[name]_[hash:6].js', //输出到build文件下，js文件夹内
         //输出路径
         path: path.join(__dirname, 'build'),
     },
     //loader 加载器，非js json文件，需要配置loader，来处理
     module: {
         rules: [{
-                test: /\.css$/, //处理已.css结尾的文件
+                test: /\.css$/i, //处理已.css结尾的文件
                 //使用那些loader进行处理
                 use: [ //use数组中loader执行顺序，从右到左，从下到上执行
-                    //创建style标签，将js里的css字符串，添加到head里
-                    "style-loader",
-                    //将css转成js字符串
-                    "css-loader"
+                    { //将css样式写到css文件里，这个区别style-loader，style-loader是将css写进html-head内
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            esModule: false
+                        }
+                    },
+                    //"style-loader",//创建style标签，将js里的css字符串，添加到head里
+                    "css-loader" //将css转成js字符串
                 ]
             },
             {
-                //处理图片资源，这个有一个缺点，就是不处理html中img标签引用的资源，只处理css里的图片资源
-                test: /\.(jpg|png|gif)$/,
-                //使用url-loader，需要下两个包，一个是url-loader，还有一个是file-loader
-                loader: 'file-loader',
-                //这个loader的配置
-                options: {
-                    //limit意思，图片大小小于8kb，就会被base64处理
-                    //优点：减少请求数量（减轻服务器压力）
-                    //缺点：图片体积会更大（文件请求速度更慢）
-                    //打包结果，一开始有两个图片，一个是500kb的，另一个是4kb的，打包后，4kb的会被转为base64
-                    // limit: 8 * 1024,
-                    name: 'static/[name].[ext]',
-                    esModule: false, //不显示图片问题，这个是原因url-loader、file-loader都是采用es6语法规范的，而不是commonjs规范，由于url-loader、file-loader中可以通过esModule属性来选择是否关闭es6语法规范,加esModule: false 和 type: 'javascript/auto' 解决打包后css里图片资源路径不正确的问题
-                },
-                type: 'javascript/auto'
+                //file-loader 和 url-loader 负责，处理css里的图片资源，其中url-loader包含file-loader
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                // webpack 5最新处理方式，这种方式是webpack5自身内置的功能
+                // type: 'asset/resource',
+                // generator: {
+                //     filename: 'images/[hash:10][ext][query]'
+                // }
+                // webpack 旧版处理方式，webpack5处理图片时，现在不建议再用url-loader 和 file-loader来处理了，如果坚持用，需要加type: 'javascript/auto' 和 esModule: false
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        outputPath: "images",
+                        name: "[hash].[ext]",
+                        limit: 1024 * 8, //单位byte，当图片小于8kb，将图片转换成base64码
+                        esModule: false //因为url-loader 和 file-loader是es6标准，不是commonjs标准
+                    }
+                }],
+                type: 'javascript/auto' // webpack5处理图片时，现在不建议再用url-loader 和 file-loader来处理了，如果坚持用，需要加上type: 'javascript/auto'
             },
             {
-                test: /\.html$/,
                 // html-loader可以处理html中的img图片，可负责将其中的图片引入，然后交由url-loader进行解析
+                test: /\.html$/i,
                 loader: 'html-loader',
                 options: {
                     esModule: false
@@ -55,15 +73,25 @@ module.exports = {
                 type: 'javascript/auto'
             },
             {
-                //打包其它资源（除了html/js/css/less等以外的资源）
-                exclude: /\.(css|less|html|js|json|jpg|png|gif)$/,
+                //打包其它资源（除了html/js/css/less等以外的资源，如字体文件,音频文件，视频文件等）
+                exclude: /\.(css|less|sass|html|js|json|png|svg|jpg|jpeg|gif)$/,
                 loader: 'url-loader',
+                options: {
+                    outputPath: "static",
+                    limit: 1024 * 8, //单位byte，当图片小于8kb，将文件转换成base64码
+                    esModule: false
+                },
                 type: 'javascript/auto'
             }
         ]
     },
     //插件
     plugins: [
+        new CleanWebpackPlugin(), //重新构建时，会自动清除，output.path路径内的文件
+        new MiniCssExtractPlugin({ //将css提取成文件，需要配合loader一起使用，详细看文档
+            linkType: "text/css",
+            filename: "css/[name]-[contenthash:6].css"
+        }),
         new HtmlWebpackPlugin({
             //网页的小icon
             favicon: "./src/static/favicon.ico",
@@ -89,18 +117,36 @@ module.exports = {
             hot: true
         }),
     ],
-    //开发服务器devServer，只会在内存中编译打包，不会有任何输出
+    //开发服务器devServer，特点，只会在内存中编译打包，不会有任何输出，可以（自动编译，自动打开浏览器，自动刷新浏览器等）
     //启动devServer指令：npx webpack serve
     devServer: {
-        //项目打包后的目录地址
-        static: path.join(__dirname, 'build'),
-        //启动gzip压缩
-        compress: true,
-        //端口号
-        port: 3000,
-        //自动打开浏览器
-        open: true
+        static: path.join(__dirname, 'build'), //项目打包后的目录地址
+
+        hot: true,
+
+        compress: true, //启动gzip压缩
+
+        port: 3000, //端口号
+
+        open: true, //自动打开浏览器
+
+        watchFiles: [
+            "./src/pages/page1/index.html",
+            "./src/pages/page1/index.css",
+            "./src/pages/page2/index.html",
+            "./src/pages/page2/index.css"
+        ],
+    },
+    //performance: {}出现下面这个，是为了解决，webpack检测到，打包出来的文件体积太大，报警告的问题（这样写，是为了提高报错门槛）
+    performance: {
+        hints: "warning",
+        maxAssetSize: 300000, // 整数类型（以字节为单位）
+        maxEntrypointSize: 500000, // 整数类型（以字节为单位）
+        assetFilter: function(assetFilename) {
+            // 只给出js与css文件的性能提示
+            return assetFilename.endsWith('.css') || assetFilename.endsWith('.js');
+        }
     },
     //打包模式
-    mode: 'production', //development 开发模式   production 生产
+    mode: 'development', //development 开发模式   production 生产
 }
